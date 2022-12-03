8dc4900f82c56615da85f8309bf67aff292e22a1
JoeJoeJoe881 <2101670D@student.tp.edu.sg>
2022-12-03 17:19:52 +0800

    Preparing list of SQL prepared statements to perform CRUD to db + Implementing getConnection method to facilitate connection to DB via JDBC

--- src/main/java/UserServlet.java
+++ src/main/java/UserServlet.java
@@ -1,6 +1,15 @@
 
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +22,32 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UserServlet")
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	//Step 1: Prepare list of variables used for database connections
+	private String jdbcURL = "jdbc:mysql://localhost:3306/devops";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "password";
+	
+	//Step 2: Prepare list of SQL prepared statements to perform CRUD to our database
+	private static final String INSERT_USERS_SQL = "INSERT INTO UserInformation" + "(name, password, email, language) VALUES" + "(?,?,?);";
+	private static final String SELECT_USER_BY_ID = "select name, password, email, language from UserInformation where name=?";
+	private static final String DELETE_USERS_SQL = "delete from UserInformation where name = ?;";
+	private static final String UPDATE_USERS_SQL = "update UserInformation set name = ?, password = ?, email = ?, language = ? where name = ?;";
+	
+	//Step 3: Implement the getConnection method which facilitates connection to the database via JDBC
+	protected Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jbdc.Driver");
+			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword)
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
        
     /**
      * @see HttpServlet#HttpServlet()
