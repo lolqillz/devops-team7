f14acecae5f06b0b50dd96e856286e9f548f04e4
JoeJoeJoe881 <2101670D@student.tp.edu.sg>
2022-12-05 13:14:46 +0800

    Implement deleteUser function

--- src/main/java/UserServlet.java
+++ src/main/java/UserServlet.java
@@ -164,6 +164,22 @@ public class UserServlet extends HttpServlet {
 		//Step 3: redirect back to UserServlet (note: remember to change the url to your project name)
 		response.sendRedirect("http://localhost:8090/HelloWorldJavaEE/UserServlet/dashboard");
 	}
+	
+	//method to delete user
+	private void deleteUser(HttpServletRequest request, HttpServletResponse response)
+	throws SQLException, IOException {
+		//Step 1: Retrieve value from the request
+		String name = request.getParameter("name");
+		
+		//Step 2: Attempt connection with database and execute delete user SQL query
+		try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL);) {
+			statement.setString(1, name);
+			int i = statement.executeUpdate();
+		}
+		
+		//Step 3: redirect back to UserServlet dashboard (note: remember to change the url to your project name)
+		response.sendRedirect("http://localhost:8090/HelloWorldJavaEE/UserServlet/dashboard");
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
