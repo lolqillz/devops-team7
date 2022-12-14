c561d5a80e3d3f028ef537076338597b2052d171
hadifurqan <hadifurqan1730@gmail.com>
2022-12-14 20:11:46 +0800

    created a servlet to connect data collected to database and print a success screen.

--- src/main/java/com/dvops/maven/eclipse/CommentServlet.java
+++ src/main/java/com/dvops/maven/eclipse/CommentServlet.java
@@ -37,15 +37,16 @@ public class CommentServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
-		String n = request.getParameter("userName");
+		String n = request.getParameter("name");
 		String c = request.getParameter("comment");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection(
-			"jdbc:mysql://localhost:3306/usercomments", "root", "password");
+			"jdbc:mysql://localhost:3306/usercomment", "root", "password");
 			PreparedStatement ps = con.prepareStatement("insert into COMMENTS values(?,?)");
 			ps.setString(1, n);
 			ps.setString(2, c);
@@ -63,6 +64,10 @@ public class CommentServlet extends HttpServlet {
 			}
 			doGet(request, response);
 		doGet(request, response);
+		
+
+//			doGet(request, response);
+//		doGet(request, response);
 		
 	}
 	
