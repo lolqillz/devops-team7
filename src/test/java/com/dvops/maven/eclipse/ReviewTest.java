417929470d756f38ac1b801e6a100c200304f50e
lolqillz <darrelgoh07@gmail.com>
2023-01-19 14:36:11 +0800

    Added dependencies for JUnit and JUnit test case
    - pom.xml updated
    - Added ReviewTest.java (JUnit test case)

---
+++ src/test/java/com/dvops/maven/eclipse/ReviewTest.java
@@ -0,0 +1,59 @@
+package com.dvops.maven.eclipse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ReviewTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBook_id() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetBook_id() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetReview() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetReview() {
+		fail("Not yet implemented");
+	}
+
+}
