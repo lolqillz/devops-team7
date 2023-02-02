38c6232f502bec6dff5dc09df7c1523684704645
hadifurqan <hadifurqan1730@gmail.com>
2023-02-02 22:34:48 +0800

    Changed URL

--- src/test/java/com/dvops/maven/eclipse/SeleniumTest.java
+++ src/test/java/com/dvops/maven/eclipse/SeleniumTest.java
@@ -29,7 +29,7 @@
 
 		@Test
 		public void checkRegisterTitle() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/register.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/register.jsp");
 			assertEquals(webDriver.getTitle(),"Register Deployment");
 			System.out.println("title: " + webDriver.getTitle());
 			
@@ -37,7 +37,7 @@
 		
 		@Test
 		public void checkRegisterId() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/register.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/register.jsp");
 			WebElement we = webDriver.findElement(By.id("form-content"));
 			System.out.println("Id WebElement: " + we.getAttribute("role"));
 			assertEquals(we.getAttribute("role"), "registerform");
@@ -45,7 +45,7 @@
 		
 		@Test
 		public void checkLoginTitle() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/login.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/login.jsp");
 			assertEquals(webDriver.getTitle(), "Login");
 			System.out.println("title: " + webDriver.getTitle());
 			
@@ -53,7 +53,7 @@
 		
 		@Test
 		public void checkLoginId() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/login.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/login.jsp");
 			WebElement we = webDriver.findElement(By.id("form-content"));
 			System.out.println("Id WebElement: " + we.getAttribute("role"));
 			assertEquals(we.getAttribute("role"), "loginform");
@@ -61,7 +61,7 @@
 		
 		@Test
 		public void checkReviewManagementTitle() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/ReviewServlet");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/ReviewServlet");
 			assertEquals(webDriver.getTitle(), "Review Management");
 			System.out.println("title: " + webDriver.getTitle());
 		
@@ -69,7 +69,7 @@
 		
 		@Test
 		public void checkReviewManagementId() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/ReviewServlet");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/ReviewServlet");
 			WebElement we = webDriver.findElement(By.id("reviewTable"));
 			System.out.println("Id WebElement: " + we.getAttribute("role"));
 			assertEquals(we.getAttribute("role"), "reviewList");
@@ -77,14 +77,14 @@
 		
 		@Test
 		public void checkCreateReviewFormTitle() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/create_review.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/create_review.jsp");
 			assertEquals(webDriver.getTitle(), "Create Review Form");
 			System.out.println("title: " + webDriver.getTitle());
 		}
 		
 		@Test
 		public void checkCreateReviewFormId() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/create_review.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/create_review.jsp");
 			WebElement we = webDriver.findElement(By.id("form-content"));
 			System.out.println("Id WebElement: " + we.getAttribute("role"));
 			assertEquals(we.getAttribute("role"), "createReviewForm");
@@ -92,7 +92,7 @@
 		
 		@Test
 		public void checkUserManagementTitle() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/userManagement.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/userManagement.jsp");
 			assertEquals(webDriver.getTitle(), "Test");
 			System.out.println("title: " + webDriver.getTitle());
 			webDriver.findElement(By.className("btn-success")).click();
@@ -102,7 +102,7 @@
 		
 		@Test
 		public void checkUserManagementId() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/userManagement.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/userManagement.jsp");
 			WebElement we = webDriver.findElement(By.id("userTable"));
 			System.out.println("Id WebElement: " + we.getAttribute("role"));
 			assertEquals(we.getAttribute("role"), "userList");
@@ -110,14 +110,14 @@
 		
 		@Test
 		public void checkIndexTitle() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/index.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/index.jsp");
 			assertEquals(webDriver.getTitle(), "Index");
 			System.out.println("title: " + webDriver.getTitle());
 		}
 		
 		@Test
 		public void checkIndexId() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/index.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/index.jsp");
 			WebElement we = webDriver.findElement(By.id("form-content"));
 			System.out.println("Id WebElement: " + we.getAttribute("role"));
 			assertEquals(we.getAttribute("role"), "indexform");
@@ -125,7 +125,7 @@
 		
 		@Test
 		public void checkUserEditTest() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/userEdit.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/userEdit.jsp");
 			assertEquals(webDriver.getTitle(), "User Management Application");
 			System.out.println("title: " + webDriver.getTitle());
 			webDriver.findElement(By.className("nav-link")).click();
@@ -135,7 +135,7 @@
 		
 		@Test
 		public void checkUserEditId() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/userEdit.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/userEdit.jsp");
 			WebElement we = webDriver.findElement(By.id("user-edits"));
 			System.out.println("Id WebElement: " + we.getAttribute("role"));
 			assertEquals(we.getAttribute("role"), "editForms");
@@ -143,7 +143,7 @@
 		
 		@Test
 		public void checkUserEditTitle() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/userEdit.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/userEdit.jsp");
 			
 			System.out.println("title: " + webDriver.getTitle());
 			assertEquals(webDriver.getTitle(), "User Management Application");
@@ -151,7 +151,7 @@
 		
 		@Test
 		public void checkCommentManagementTitle() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/CommentServlet");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/CommentServlet");
 			assertEquals(webDriver.getTitle(), "Comment Management");
 			System.out.println("title: " + webDriver.getTitle());
 			webDriver.findElement(By.className("btn-success")).click();
@@ -161,7 +161,7 @@
 		}
 		@Test
 		public void checkCreateCommentTitle() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/create_comment.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/create_comment.jsp");
 			
 			System.out.println("title: " + webDriver.getTitle());
 			assertEquals(webDriver.getTitle(), "Create Comment Form");
@@ -169,7 +169,7 @@
 		
 		@Test
 		public void checkCommentManagementId() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/CommentServlet");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/CommentServlet");
 			WebElement we = webDriver.findElement(By.id("commentTable"));
 			System.out.println("Id WebElement: " + we.getAttribute("role"));
 			assertEquals(we.getAttribute("role"), "commentList");
@@ -177,7 +177,7 @@
 		
 		@Test
 		public void checkCreateCommentFormTitle() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/create_comment.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/create_comment.jsp");
 			
 			System.out.println("title: " + webDriver.getTitle());
 			assertEquals(webDriver.getTitle(), "Create Comment Form");
@@ -185,7 +185,7 @@
 		
 		@Test
 		public void checkCreateCommentFormId() {
-			webDriver.navigate().to("http://localhost:8090/HelloWorldJavaEE/create_comment.jsp");
+			webDriver.navigate().to("http://localhost:8080/HelloWorldJavaEE/create_comment.jsp");
 			WebElement we = webDriver.findElement(By.id("form-content"));
 			System.out.println("Id WebElement: " + we.getAttribute("role"));
 			assertEquals(we.getAttribute("role"), "createCommentForm");
